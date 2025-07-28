af700e795e0a88379641e65affbf1b95454e882b
Melany <123525805+melanyscode@users.noreply.github.com>
2025-07-28 17:43:08 -0600

    session server state

---
+++ AdvancedProgramming/AdvancedProgramming.Mvc/Helpers/SessionState.cs
@@ -0,0 +1,30 @@
+﻿using AdvancedProgramming.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdvancedProgramming.Mvc.Helpers
+{
+    public class SessionState
+    {
+        private HttpSessionStateBase session;
+
+        public SessionState(HttpSessionStateBase session)
+        {
+            this.session = session;
+        }
+
+        public User CurrentUser
+        {
+            get => session["User"] as User;
+            set => session["User"] = value;
+        }
+
+        public void Logout()
+        {
+            session.Remove("User");
+        }
+    }
+
+}
